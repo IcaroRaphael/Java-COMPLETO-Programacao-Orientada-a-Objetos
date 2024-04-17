fd6b42ff134efb60cc8a134734e7cc4a5a2dcb47
IcaroRaphael <icaro2551@gmail.com>
2024-04-17 17:30:20 -0300

    Seção 08 (Membros estáticos - Parte 1)

---
+++ java-completo/src/secao08/aula070/util/Calculator.java
@@ -0,0 +1,14 @@
+package secao08.aula070.util;
+
+public class Calculator {
+
+    public final double PI = 3.14159;
+
+    public double circumference(double radius){
+        return 2 * PI * radius;
+    }
+
+    public double volume(double radius){
+        return 4 * PI * Math.pow(radius, 3) / 3;
+    }
+}
