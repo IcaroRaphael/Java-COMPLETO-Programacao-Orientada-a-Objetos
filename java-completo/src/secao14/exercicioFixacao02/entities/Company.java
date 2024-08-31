2121c6c0518199a88faa32197b17cf9be3fad90c
IcaroRaphael <icaro2551@gmail.com>
2024-08-30 22:14:21 -0300

    Seção 14 (Exercício de fixação)

---
+++ java-completo/src/secao14/exercicioFixacao02/entities/Company.java
@@ -0,0 +1,34 @@
+package secao14.exercicioFixacao02.entities;
+
+public class Company extends TaxPayer{
+    private int numberOfEmployees;
+
+    public Company(){
+        super();
+    }
+
+    public Company(String name, Double anualIncome, int numberOfEmployees) {
+        super(name, anualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public double tax() {
+        double value = 0;
+        if(numberOfEmployees <= 10){
+            value += 0.16 * super.getAnualIncome();
+        }
+        else{
+            value += 0.14 * super.getAnualIncome();
+        }
+        return value;
+    }
+}
