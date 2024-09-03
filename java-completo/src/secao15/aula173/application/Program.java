56b73853d20198816b9669cc6f89934e023a94db
IcaroRaphael <icaro2551@gmail.com>
2024-09-03 15:50:36 -0300

    Seção 15 (Bloco finally)

---
+++ java-completo/src/secao15/aula173/application/Program.java
@@ -0,0 +1,27 @@
+package secao15.aula173.application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Home\\Documents\\GitHub\\Java-COMPLETO-Programacao-Orientada-a-Objetos\\java-completo\\src\\secao15\\aula173\\application\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("Finally block executed");
+        }
+    }
+}
