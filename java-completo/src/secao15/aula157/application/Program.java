b077af4d8e5832350abe3a19eba9f3bfd4f17ce2
Ícaro Raphael Queiroz Cavalcante <icaro2551@gmail.com>
2024-09-23 15:47:05 -0300

    Seção 15 (Bloco try-with-resources)

---
+++ java-completo/src/secao15/aula157/application/Program.java
@@ -0,0 +1,23 @@
+package secao15.aula157.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\magna\\Documents\\GitHub\\Java-COMPLETO-Programacao-Orientada-a-Objetos\\java-completo\\src\\secao15\\aula157\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+
+            while (line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
