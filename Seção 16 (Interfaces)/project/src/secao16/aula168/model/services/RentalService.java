edf36d94a8700721105f2fc9d871b7bf51613610
Ícaro Raphael Queiroz Cavalcante <icaro2551@gmail.com>
2024-09-30 18:35:48 -0300

    Seção 16 (Solução do problema - PARTE 3)

---
+++ Seção 16 (Interfaces)/project/src/secao16/aula168/model/services/RentalService.java
@@ -0,0 +1,36 @@
+package secao16.aula168.model.services;
+
+import secao16.aula168.model.entities.CarRental;
+import secao16.aula168.model.entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+    private Double princePerDay;
+    private Double pricePerHour;
+    private TaxService taxService;
+
+    public RentalService(Double princePerDay, Double pricePerHour, TaxService taxService) {
+        this.princePerDay = princePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental){
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes/60;
+
+        double basicPayment;
+        if(hours <= 12){
+            basicPayment = pricePerHour * Math.ceil(hours);
+        }
+        else {
+            basicPayment = princePerDay * Math.ceil(hours / 24.0);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
---
+++ Seção 16 (Interfaces)/project/src/secao16/aula168/model/services/TaxService.java
@@ -0,0 +1,5 @@
+package secao16.aula168.model.services;
+
+public interface TaxService {
+    double tax(double amount);
+}
