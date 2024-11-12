5b33f1c337f8b39ce8eb213d689135dabfc0982b
IcaroRaphael <icaro2551@gmail.com>
2024-11-11 23:53:18 -0300

    Seção 18 (Consumer)

---
+++ Seção 18 (Programação funcional e expressões lambda)/project/src/secao18/aula197/util/PriceUpdate.java
@@ -0,0 +1,12 @@
+package secao18.aula197.util;
+
+import secao18.aula197.entities.Product;
+
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product> {
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+}
